effba0d9613df932d574189fd498d8b3ee5a9c4f
RGGH <pisscompany@gmail.com>
2023-08-30 16:29:42 +0100

    2

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
+use colored::Colorize;
 use std::fs;
+use std::io::{stdin, Read};
 use std::path::Path;
-use std::io::{stdin, Write};
-use colored::Colorize; // Assuming you're using the `colored` crate for text coloring
 
 fn main() {
+    // Network Inteface Location (Ubuntu/Linux Mint)
     let net = Path::new("/sys/class/net");
     let entry = fs::read_dir(net).expect("Error");
+
     let ifaces = entry
         .filter_map(|p| p.ok())
         .map(|p| p.path().file_name().expect("Error").to_os_string())
@@ -13,36 +15,21 @@ fn main() {
         .collect::<Vec<String>>();
 
     println!("These are the available interfaces: {:?}", ifaces);
+    println!("\nChoose an interface: ");
+    // Create a mutable String and store the user's input
+    let mut input = String::new();
+    stdin().read_line(&mut input).expect("Failed to read line");
 
-    let mut iface_path: Option<std::path::PathBuf> = None;
+    // !! important - remove newline character from input
+    //let iface_choice = input;//.trim();
+    let iface_choice = input.trim();
+    // Construct the path based on the chosen interface
+    let iface_path = net.join(iface_choice).join("address");
 
-    while iface_path.is_none() {
-        println!("\nChoose an interface: ");
-
-        // Create a mutable String and store the user's input
-        let mut input = String::new();
-        stdin().read_line(&mut input).expect("Failed to read line");
-
-        // !! important - remove newline character from input
-        let iface_choice = input.trim();
-        // Construct the path based on the chosen interface
-        let potential_iface_path = net.join(iface_choice).join("address");
-
-        // Check if the potential path exists (i.e., if the interface choice is valid)
-        if potential_iface_path.exists() {
-            iface_path = Some(potential_iface_path);
-        } else {
-            println!("Invalid interface name. Please choose from the available interfaces.");
-        }
-    }
-
-    if let Some(iface_path) = iface_path {
-        println!("Chosen interface path: {:?}", iface_path);
-
-        let mut f = fs::File::open(&iface_path).expect("Failed");
-        let mut macaddr = String::new();
-        f.read_to_string(&mut macaddr).expect("Error");
-        println!("MAC address: {}", macaddr.trim().green());
-    }
+    // Now you can use `iface_path` to further work with the chosen interface's path
+    println!("Chosen interface path: {:?}", iface_path);
+    let mut f = fs::File::open(iface_path).expect("Failed");
+    let mut macaddr = String::new();
+    f.read_to_string(&mut macaddr).expect("Error");
+    println!("MAC address: {}", macaddr.to_string().green());
 }
-
